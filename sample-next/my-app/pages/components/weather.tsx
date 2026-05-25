40ff8e11310690fb92469c9d7fce1c05a1a7c547
wolfy <wolfy@shitwolfymakes.com>
2026-05-25 16:13:06 -0500

    Update weather.tsx

--- sample-next/my-app/pages/components/weather.tsx
+++ sample-next/my-app/pages/components/weather.tsx
@@ -8,6 +8,10 @@ const PageComponentWeather = () => {
 
     const WeatherComponent = (props: WeatherProps) => {
         const [count, setCount] = useState(0);
+        // This runs on every render, but because the deps are [],
+        // it sees nothing needing changing on subsequent re-renders
+        // Effects run *AFTER* React commits the render to the DOM,
+        // so on pageload count is rendered as 0 for a moment
         useEffect(() => {
             setCount(1);
         }, []);
