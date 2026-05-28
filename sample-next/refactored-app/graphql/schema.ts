870968556f47be5e3528f6b13d198835385f4633
wolfy <wolfy@shitwolfymakes.com>
2026-05-28 15:43:32 -0500

    add typedefs

--- sample-next/refactored-app/graphql/schema.ts
+++ sample-next/refactored-app/graphql/schema.ts
@@ -1,1 +1,33 @@
 import gql from "graphql-tag";
+
+export const typeDefs = gql`
+
+    type FriendsType {
+        zip: String!
+        weather: String!
+    }
+    
+    type LocationWeatherType {
+        zip: String!
+        weather: String!
+        tempC: String!
+        tempF: String!
+        friends: [FriendsType]!
+    }
+    
+    type LocationWeatherInput {
+        zip: String!
+        weather: String
+        tempC: String
+        tempF: String
+        friends: [String]
+    }
+
+    type Query {
+        weather(zip: String): [LocationWeatherType]!
+    }
+
+    type Mutator {
+        weather(data: LocationWeatherInput): [LocationWeatherType]!
+    }
+`;
