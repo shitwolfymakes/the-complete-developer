bf3cc3c197d70b07a71b50a12738879d816ea93d
wolfy <wolfy@shitwolfymakes.com>
2026-06-08 17:26:43 -0500

    Create model.ts

---
+++ sample-next/refactored-app/mongoose/weather/model.ts
@@ -0,0 +1,5 @@
+import mongoose, { model } from "mongoose";
+import { WeatherInterface } from "./interface";
+import { WeatherSchema } from "./schema";
+
+export default mongoose.models.Weather || model<WeatherInterface>("weather", WeatherSchema);
