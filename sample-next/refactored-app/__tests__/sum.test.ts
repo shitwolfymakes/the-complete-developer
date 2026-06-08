1424fc9b164139bacadce60f762dad0b19c13e08
wolfy <wolfy@shitwolfymakes.com>
2026-06-08 18:55:04 -0500

    add jest tst

---
+++ sample-next/refactored-app/__tests__/sum.test.ts
@@ -0,0 +1,6 @@
+import { describe } from "node:test";
+import { sum } from "../helpers/sum";
+
+describe("the sum function", () => {
+    
+});
---
+++ sample-next/refactored-app/helpers/sum.ts
@@ -0,0 +1,2 @@
+const sum = () => {};
+export { sum };
