3f90621746488314aa4a73aeae27f3b6e4e652df
wolfy <wolfy@shitwolfymakes.com>
2026-06-08 18:22:57 -0500

    Update resolvers.ts

--- sample-next/refactored-app/graphql/resolvers.ts
+++ sample-next/refactored-app/graphql/resolvers.ts
@@ -1,4 +1,5 @@
 import { db } from "./data";
+import { findByZip, updateByZip } from "../mongoose/weather/services";
 
 declare interface WeatherInterface {
     zip: string;
@@ -11,12 +12,15 @@ declare interface WeatherInterface {
 export const resolvers = {
     Query: {
         weather: async (_: any, param: WeatherInterface) => {
-            return [db.find((item) => item.zip === param.zip)];
+            let data = await findByZip(param.zip);
+            return [data];
         }
     },
     Mutation: {
         weather: async (_: any, param: { data: WeatherInterface}) => {
-            return [db.find((item) => item.zip === param.data.zip)];
+            await updateByZip(param.data.zip, param.data);
+            let data = await findByZip(param.data.zip);
+            return [data];
         }
     }
 };
