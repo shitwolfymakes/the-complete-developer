1372a3618e5b557fb447d1f23c1ec437851684be
wolfy <wolfy@shitwolfymakes.com>
2026-06-08 17:44:08 -0500

    Update services.ts

--- sample-next/refactored-app/mongoose/weather/services.ts
+++ sample-next/refactored-app/mongoose/weather/services.ts
@@ -1,11 +1,22 @@
 import WeatherModel from "./model";
 import { WeatherInterface } from "./interface";
 
-export async function storeDocument(doc: WeatherInterface): Promise<boolean> {
+export async function storeDocument(doc: WeatherInterface): 
+    Promise<boolean> {
     try {
         await WeatherModel.create(doc)
-    } catch (error) {
+    } catch (err) {
         return false;
     }
     return true;
+}
+
+export async function findByZip(paramZip: string): 
+    Promise<Array<WeatherInterface> | null> {
+    try {
+        return await WeatherModel.findOne({zip: paramZip});
+    } catch (err) {
+        console.log(err);
+    }
+    return [];
 }
