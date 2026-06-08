02a47c762d066273ed828059f8437e67b56ced6f
wolfy <wolfy@shitwolfymakes.com>
2026-06-08 18:18:29 -0500

    fix the lookup timing issue

--- sample-next/refactored-app/pages/api/v1/weather/[zipcode].ts
+++ sample-next/refactored-app/pages/api/v1/weather/[zipcode].ts
@@ -1,12 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { findByZip } from "../../../../mongoose/weather/services";
 import dbConnect from "./../../../../middleware/db-connect";
-dbConnect();
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ): Promise<NextApiResponse<WeatherDetailType> | void> {
+    await dbConnect(); // put here so the db seed is completed BEFORE the lookup
     let data = await findByZip(req.query.zipcode as string);
     return res.status(200).json(data);
 }
