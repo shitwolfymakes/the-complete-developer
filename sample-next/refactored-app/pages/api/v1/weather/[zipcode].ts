3c5ec5d7237cf5feaae6fe958f233bd1ba934b76
wolfy <wolfy@shitwolfymakes.com>
2026-06-08 17:52:01 -0500

    Update [zipcode].ts

--- sample-next/refactored-app/pages/api/v1/weather/[zipcode].ts
+++ sample-next/refactored-app/pages/api/v1/weather/[zipcode].ts
@@ -1,12 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { findByZip } from "../../../../mongoose/weather/services";
+import dbConnect from "./../../../../middleware/db-connect";
+dbConnect();
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ): Promise<NextApiResponse<WeatherDetailType> | void> {
-    return res.status(200).json({
-        zipcode: req.query.zipcode,
-        weather: "sunny",
-        temp: 15
-    });
+    let data = await findByZip(req.query.zipcode as string);
+    return res.status(200).json(data);
 }
