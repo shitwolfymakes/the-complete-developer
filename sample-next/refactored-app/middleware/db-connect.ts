12db601dd34402746eaca2460e894c474857e4ed
wolfy <wolfy@shitwolfymakes.com>
2026-06-08 20:16:12 -0500

    Add spies

--- sample-next/refactored-app/middleware/db-connect.ts
+++ sample-next/refactored-app/middleware/db-connect.ts
@@ -39,6 +39,8 @@ async function dbConnect(): Promise<any | String> {
         tempF: "86F",
         friends: ["96815", "96814"],
     });
+
+    return mongoServer;
 };
 
 export default dbConnect;
