77be4599e8826832cff377fe6e2352fc5d55d18d
wolfy <wolfy@shitwolfymakes.com>
2026-06-08 21:24:54 -0500

    add e2e test

---
+++ sample-next/refactored-app/__tests__/pages/api/v1/weather/zipcode.e2e.test.ts
@@ -0,0 +1,15 @@
+/**
+ * @jest-enviroment node
+ */
+
+jest.setTimeout(20000);
+describe("The API /v1/weather/[zipcode]", () => {
+    test("returns the correct data for the zipcode 96815", async () => {
+        const zip = "96815";
+        let response = await fetch(`http://localhost:3000/api/v1/weather/${zip}`);
+        let body = await response.json();
+        expect(body.zip).toEqual(zip);
+    });
+});
+
+export {};
