6ad82fa4a46b7154d99569dde5cf9ddcc5f72a65
wolfy <wolfy@shitwolfymakes.com>
2026-06-08 19:12:54 -0500

    Update sum.test.ts

--- sample-next/refactored-app/__tests__/sum.test.ts
+++ sample-next/refactored-app/__tests__/sum.test.ts
@@ -2,18 +2,14 @@ import { sum } from "../helpers/sum";
 
 describe("the sum function", () => {
     test("two plus two is four", () => {
-        let first = 2;
-        let second = 2;
-        let expectation = 4;
-        let result = sum(first, second);
-        expect(result).toBe(expectation);
+        expect(sum([2, 2])).toBe(4);
     });
 
     test("minus eight plus four is minus four", () => {
-        let first = -8;
-        let second = 4;
-        let expectation = -4;
-        let result = sum(first, second);
-        expect(result).toBe(expectation);
+        expect(sum([-8, 4])).toBe(-4);
+    });
+
+    test("two plus two plus -4 is 0", () => {
+        expect(sum([2, 2, -4])).toBe(0);
     });
 });
