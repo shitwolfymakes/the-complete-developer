2fe7f0e37b85d7b9cb5cd52b9a04fcdb30f671e2
wolfy <wolfy@shitwolfymakes.com>
2026-06-09 15:38:24 -0500

    Create schema.ts

---
+++ part-2/code/foodfinder-application/graphql/schema.ts
@@ -0,0 +1,17 @@
+import gql from "graphql-tag";
+
+import locationTypeDefsCustom from "graphql/locations/custom.gql";
+import locationTypeDefsQueries from "graphql/locations/queries.gql";
+import locationTypeDefsMutations from "graphql/locations/mutations.gql";
+
+export const typeDefs = gql`
+    ${locationTypeDefsCustom}
+
+    type Query {
+        ${locationTypeDefsQueries}
+    }
+
+    type Mutations {
+        ${locationTypeDefsMutations}
+    }
+`;
