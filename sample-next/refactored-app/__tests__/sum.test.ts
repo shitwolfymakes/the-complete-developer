6e3deb7c12fa11951bb0299661a30bd8664ceaab
wolfy <wolfy@shitwolfymakes.com>
2026-06-08 19:03:52 -0500

    add tests and types

--- sample-next/refactored-app/__tests__/sum.test.ts
+++ sample-next/refactored-app/__tests__/sum.test.ts
@@ -1,6 +1,19 @@
-import { describe } from "node:test";
 import { sum } from "../helpers/sum";
 
 describe("the sum function", () => {
-    
+    test("two plus two is four", () => {
+        let first = 2;
+        let second = 2;
+        let expectation = 4;
+        let result = sum(first, second);
+        expect(result).toBe(expectation);
+    });
+
+    test("minus eight plus four is four", () => {
+        let first = -8;
+        let second = 4;
+        let expectation = 4;
+        let result = sum(first, second);
+        expect(result).toBe(expectation);
+    });
 });
