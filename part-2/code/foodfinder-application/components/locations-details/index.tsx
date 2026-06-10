5dc029da8294d3efdab3805e701b096b82865876
wolfy <wolfy@shitwolfymakes.com>
2026-06-10 16:44:06 -0500

    Create index.tsx

---
+++ part-2/code/foodfinder-application/components/locations-details/index.tsx
@@ -0,0 +1,38 @@
+import { LocationType } from "mongoose/locations/schema";
+import styles from "./index.module.css";
+
+interface PropsInterface {
+    location: LocationType;
+}
+
+const locationDetail = (props: PropsInterface) => {
+    let location = props.location;
+    return (
+        <div>
+            {location && (
+                <ul className={styles.root}>
+                    <li>
+                        <b>Address: </b>
+                        {location.address}
+                    </li>
+                    <li>
+                        <b>Zipcode: </b>
+                        {location.zipcode}
+                    </li>
+                    <li>
+                        <b>Borough: </b>
+                        {location.borough}
+                    </li>
+                    <li>
+                        <b>Cuisine: </b>
+                        {location.cuisine}
+                    </li>
+                    <li>
+                        <b>Grade: </b>
+                        {location.grade}
+                    </li>
+                </ul>
+            )}
+        </div>
+    );
+};
